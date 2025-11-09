1a1562bf8938eac584e76aaede9caf1b1910abf7
Ivan <svaild793@gmail.com>
2025-11-09 15:08:28 +0300

    version2(the application logic is done)

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -17,18 +17,43 @@ public class FilmController {
 
     @PostMapping("/create-film")
     public Film addFilm(@RequestBody Film film) throws ValidationException {
-
+        if (film == null) {
+            throw new ValidationException("Передан пустой объект");
+        } else if (film.getName().isEmpty()) {
+            throw new ValidationException("Название не может быть пустым");
+        } else if (film.getDescription().length() > 200) {
+            throw new ValidationException("Максимальная длинна описания 200");
+        } else if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12 ,28))) {
+            throw new ValidationException("Дата релиза должна быть не раньше 28 декабря 1895 года");
+        } else if (film.getDuration().isNegative()) {
+            throw new ValidationException("продолжительность фильма должна быть положительным числом");
+        } else {
+            return filmStorage.put(film.getId(), film);
+        }
     }
 
     @PutMapping("/update-film")
-    public Film updateFilm(@RequestBody Film film) {
-
+    public Film updateFilm(@RequestBody Film film) throws ValidationException {
+        if (film == null) {
+            throw new ValidationException("Передан пустой объект");
+        } else if (film.getName() == null) {
+            throw new ValidationException("Название не может быть пустым");
+        } else if (film.getDescription().length() > 200) {
+            throw new ValidationException("Максимальная длинна описания 200");
+        } else if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12 ,28))) {
+            throw new ValidationException("Дата релиза должна быть не раньше 28 декабря 1895 года");
+        } else if (film.getDuration().isNegative()) {
+            throw new ValidationException("Продолжительность фильма должна быть положительным числом");
+        } else {
+            filmStorage.remove(film.getId());
+            return filmStorage.put(film.getId(), film);
+        }
     }
 
     @GetMapping("/get-films")
-    public void getFilmStorage() {
+    public void getFilmStorage() throws ValidationException {
         if (filmStorage.isEmpty()) {
-
+            throw new ValidationException("Список filmStorage пуст");
         }
         for (Film film : filmStorage.values()) {
             System.out.println(film);
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,8 +2,10 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 
 @RestController
@@ -12,19 +14,44 @@ public class UserController {
     private HashMap<Integer, User> allUsers;
 
     @PostMapping("/create-user")
-    public User createUser(@RequestBody User user) {
-
+    public User createUser(@RequestBody User user) throws ValidationException {
+        if (user == null) {
+            throw new ValidationException("Передан пустой объект");
+        } else if (user.getEmail().isEmpty() || !user.getEmail().contains("@")) {
+            throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ @");
+        } else if (user.getLogin().isEmpty() || user.getLogin().contains(" ")) {
+            throw new ValidationException("Логин не может быть пустым и содержать пробелы");
+        } else if (user.getName().isEmpty() && (user.getLogin().isEmpty() || user.getLogin().contains(" "))) {
+            throw new ValidationException("Ошибка");
+        } else if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Дата рождения не может быть в будущем");
+        } else {
+            return allUsers.put(user.getId(), user);
+        }
     }
 
     @PutMapping("/update-user")
-    public User updateUser(@RequestBody User user) {
-
+    public User updateUser(@RequestBody User user) throws ValidationException {
+        if (user == null) {
+            throw new ValidationException("Передан пустой объект");
+        } else if (user.getEmail().isEmpty() || !user.getEmail().contains("@")) {
+            throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ @");
+        } else if (user.getLogin().isEmpty() || user.getLogin().contains(" ")) {
+            throw new ValidationException("Логин не может быть пустым и содержать пробелы");
+        } else if (user.getName().isEmpty() && (user.getLogin().isEmpty() || user.getLogin().contains(" "))) {
+            throw new ValidationException("Ошибка");
+        } else if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Дата рождения не может быть в будущем");
+        } else {
+            allUsers.remove(user.getId());
+            return allUsers.put(user.getId(), user);
+        }
     }
 
     @GetMapping("/get-users")
-    public void getAllUsers() {
+    public void getAllUsers() throws ValidationException {
         if (allUsers.isEmpty()){
-
+            throw new ValidationException("Список allUsers пуст");
         }
         for (User user : allUsers.values()) {
             System.out.println(user);
